9d2c5575f21dfe3c6328d52b1a5d2abcfd799361
Delfrinando Pranata <delfrinando@Delfrinandos-MacBook-Pro.local>
2023-08-31 06:50:01 +0800

    add static website

--- server.js
+++ server.js
@@ -1,100 +1,37 @@
-// before rum
-//npm install express
-
-// to run 
-// npm start
-
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-const OS = require('os');
-const ENV = 'DEV';
+// Imports
+const express = require('express')
+const app = express()
+const port = 3000
 
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from Node!';
-  res.send(getPage(msg));
-});
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/vendor', express.static(__dirname + 'public/vendor'))
 
-app.get('/test', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
-  res.send(getPage(msg));
-});
+// Set Views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.get('', (req, res) => {
+    res.render('index', { text: 'This is EJS'})
+})
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/about', (req, res) => {
+    res.render('about', { text: 'About Page'})
+})
 
-function getPage(message) {
+app.get('/meetings', (req, res) => {
+    res.render('meetings', { text: 'About Page'})
+})
 
-  let body = "<!DOCTYPE html>\n"
-    + "<html>\n"
-    + "<style>\n"
-    + "body, html {\n"
-    + "  height: 100%;\n"
-    + "  margin: 0;\n"
-    + "}\n"
-    + "\n"
-    + ".bgimg {\n"
-    + "  background-image: url('https://www.w3schools.com/w3images/forestbridge.jpg');\n"
-    + "  height: 100%;\n"
-    + "  background-position: center;\n"
-    + "  background-size: cover;\n"
-    + "  position: relative;\n"
-    + "  color: white;\n"
-    + "  font-family: \"Courier New\", Courier, monospace;\n"
-    + "  font-size: 25px;\n"
-    + "}\n"
-    + "\n"
-    + ".topleft {\n"
-    + "  position: absolute;\n"
-    + "  top: 0;\n"
-    + "  left: 16px;\n"
-    + "}\n"
-    + "\n"
-    + ".bottomleft {\n"
-    + "  position: absolute;\n"
-    + "  bottom: 0;\n"
-    + "  left: 16px;\n"
-    + "}\n"
-    + "\n"
-    + ".middle {\n"
-    + "  position: absolute;\n"
-    + "  top: 50%;\n"
-    + "  left: 50%;\n"
-    + "  transform: translate(-50%, -50%);\n"
-    + "  text-align: center;\n"
-    + "}\n"
-    + "\n"
-    + "hr {\n"
-    + "  margin: auto;\n"
-    + "  width: 40%;\n"
-    + "}\n"
-    + "</style>\n"
-    + "<body>\n"
-    + "\n"
-    + "<div class=\"bgimg\">\n"
-    + "  <div class=\"topleft\">\n"
-    + "    <p>ENVIRONMENT: " + ENV + "</p>\n"
-    + "  </div>\n"
-    + "  <div class=\"middle\">\n"
-    + "    <h1>Host/container name</h1>\n"
-    + "    <hr>\n"
-    + "    <p>" + OS.hostname() + "</p>\n"
-    + "  </div>\n"
-    + "  <div class=\"bottomleft\">\n"
-    + "    <p>" + message + "</p>\n"
-    + "  </div>\n"
-    + "</div>\n"
-    + "\n"
-    + "</body>\n"
-    + "</html>\n";
-  return body;
-}
+
+app.get('/meeting-details', (req, res) => {
+    res.render('meeting-details', { text: 'About Page'})
+})
+
+
+
+//  Listen on port 3000
+app.listen(port, () => console.info(`Listening on port ${port}`))
