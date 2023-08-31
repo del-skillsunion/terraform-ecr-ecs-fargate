93398773caa660e26a3e4c7e678685c5b8ca2f6c
Delfrinando Pranata <delfrinando@Delfrinandos-MacBook-Pro.local>
2023-08-31 14:41:25 +0800

    Add npm audit

--- command.sh
+++ command.sh
@@ -1,3 +1,5 @@
 docker build -t tf-sample-application-image .
 docker tag tf-sample-application-image:latest 255945442255.dkr.ecr.ap-southeast-1.amazonaws.com/tf-sample-application:latest
 docker push 255945442255.dkr.ecr.ap-southeast-1.amazonaws.com/tf-sample-application:latest
+
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 255945442255.dkr.ecr.ap-southeast-1.amazonaws.com
